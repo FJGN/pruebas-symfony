441ce2ec5b3c2b02400d8a85cbec2ee86d255631
Emilio <Emilio@Mountain>
2016-05-13 14:03:57 +0200

    Modificada la clase Formulario de UserBundle para redirigir peticiones.

--- src/UserBundle/Controller/FormularioController.php
+++ src/UserBundle/Controller/FormularioController.php
@@ -2,6 +2,7 @@
 
 namespace UserBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FormularioController extends Controller {
@@ -10,21 +11,35 @@ class FormularioController extends Controller {
     public function indexAction($tipo){
         
         // Redirigir peticion hacia login o registro
+        // Si no es ninguna, redirigir a pagina de error.
+        // Modificar esta clase
         
+        $response = new Response(
+                "<html><head></head><body>INCORRECTO</body></html>"
+        );
+                
+        if($tipo == 'login'){
+            $response = $this->loginAction();
+        }
+        elseif($tipo == 'registro'){
+            $response = $this->registroAction();
+        }
+        
+        return $response;
     }
 
     
     public function loginAction(){
         return $this->render(
                 'usuarios/formulario.html.twig',
-                array('tipo' => 'Login')
+                array('tipo_formulario' => 'Login')
         );
     }
     
     public function registroAction(){
         return $this->render(
                 'usuarios/formulario.html.twig',
-                array('tipo' => 'Registro')
+                array('tipo_formulario' => 'Registro')
         );
     }
     
