1e7dc9ede706c7b4a1d1705787c38d56b26333f3
Emilio <Emilio@Mountain>
2016-05-23 01:11:12 +0200

    Acortado el fichero FormularioController

--- src/UserBundle/Controller/FormularioController.php
+++ src/UserBundle/Controller/FormularioController.php
@@ -5,6 +5,7 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use UserBundle\Entity\Usuario;
+use UserBundle\Form\UserType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -23,24 +24,9 @@ class FormularioController extends Controller {
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        
-        $form = $this->createFormBuilder(new Usuario())
-                ->add('username', TextType::class, array('label' => 'Usuario'))
-                ->add('password', PasswordType::class, array('label' => 'Contraseña'))
-                ->add('roles', ChoiceType::class, array('label' => 'Selecciona un rol',
-                        'choices' => array(
-                            'Usuario' => 'user',
-                            'Administrador' => 'admin',
-                            'Root' => 'root'
-                        ),
-                        'expanded' => true,
-                        'multiple' => false,
-                        'data' => 'user'
-                ))->getForm();
-        
-        
-        
-        
+                
+        $form = $this->createForm(UserType::class, new Usuario());
+                                
         return $this->render(
                 'usuarios/formulario.html.twig', array(
                 'tipo_formulario' => 'Login',
@@ -55,41 +41,25 @@ class FormularioController extends Controller {
     public function registroAction(Request $request) {
 
 
-        $usuario = new Usuario();
-        $form = $this->createFormBuilder($usuario)
-                ->add('username', TextType::class, array('label' => 'Usuario'))
-                ->add('password', PasswordType::class, array('label' => 'Contraseña'))
-                ->add('roles', ChoiceType::class, array('label' => 'Selecciona un rol',
-                        'choices' => array(
-                            'Usuario' => 'user',
-                            'Administrador' => 'admin',
-                            'Root' => 'root'
-                        ),
-                        'expanded' => true,
-                        'multiple' => false,
-                        'data' => 'user'
-                ))->getForm();
+        $usuario = new Usuario();               
+        $form = $this->createForm(UserType::class, $usuario);
         
-        $error = null;        
-        
-        
+        $error = null;                        
         $last_username = ($form->get('username')->getData() == null)?'':$form->get('username')->getData();
         
         $form->handleRequest($request);
-
-        var_dump("joioo");
+        
         if ($form->isValid() && $form->isSubmitted()) {
             $data = array(
                     'usuario' => $form->get('username')->getData(),
-                    'contra' => $form->get('password')->getData(),
+                    'contra' => $form->get('plainPassword')->getData(),
                     'rol' => $form->get('roles')->getData()
             );
             
             
             $signup_success = true;
 
-
-            var_dump("joioo");
+            
             // ================================================
             // check user if exists
             // false: show errors
@@ -101,7 +71,7 @@ class FormularioController extends Controller {
 
                 $password = $this->get('security.password_encoder')
                         ->encodePassword($usuario, $usuario->getPlainPassword());
-                $usuario->setPassword($password);
+                $usuario->setPassword($password);                
                 
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($usuario);
@@ -117,7 +87,6 @@ class FormularioController extends Controller {
         }
         
         
-        
         return $this->render(
                 'usuarios/formulario.html.twig', array(
                 'tipo_formulario' => 'Registrar',
@@ -130,90 +99,4 @@ class FormularioController extends Controller {
     }
 
     
-    
-    
-    
-    
-    
-    ///////////////////////////////////////////////////////////
-    // FUNCION ANTERIOR!!!!!!!!!
-    ///////////////////////////////////////////////////////////
-    public function formAction($tipo, Request $request) {
-
-        if ($tipo == 'login' || $tipo == 'registro') {
-
-
-
-            // if user already login/register, show other page                                    
-
-            $tipo_formulario = ($tipo == 'login') ? 'Login' : 'Registrar';
-
-            $form = $this->createFormBuilder(new Usuario())
-                    ->add('user', TextType::class, array('label' => 'Usuario'))
-                    ->add('password', PasswordType::class, array('label' => 'Contraseña'));
-
-
-            if ($tipo_formulario == 'Registrar') {
-                
-            }
-
-            $form = $form->add('enviar', SubmitType::class, array('label' => $tipo_formulario))
-                    ->getForm();
-
-
-            $form->handleRequest($request);
-
-            if ($form->isValid()) {
-                $data = array(
-                    'usuario' => $form->get('user')->getData(),
-                    'contra' => $form->get('password')->getData()
-                );
-
-                if ($tipo_formulario == 'Registrar') {
-                    array_push($data, array('rol' => $form->get('rol')->getData()));
-
-                    $signup_success = true;
-
-
-                    // ================================================
-                    // check user if exists
-                    // false: show errors
-                    // true: save data and redirect to success page
-                    // ================================================
-
-                    if ($signup_success) {
-                        // Save data and change session to this user
-
-                        return $this->render('usuarios/exito.html.twig', $data);
-                    } else {
-                        // Errors
-                    }
-                } else {
-                    $login_success = true;
-
-                    // ================================================
-                    // check user
-                    // false: show errors
-                    // true: redirect to index page
-                    // ================================================
-
-                    if ($login_success) {
-                        return $this->redirect('inicio');
-                    } else {
-                        // Errors
-                    }
-                }
-            }
-
-            return $this->render(
-                            'usuarios/formulario.html.twig', array(
-                        'tipo_formulario' => $tipo_formulario,
-                        'formulario' => $form->createView()
-                            )
-            );
-        } else {
-            throw $this->createNotFoundException('La página solicitada no existe');
-        }
-    }
-
 }
