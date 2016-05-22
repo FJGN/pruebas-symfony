6e81b40ec0e39ce7da45cafdb531fd538d6ccea0
Francisco Javier González Navarro <f.j.gonzalez.n@gmail.com>
2016-05-22 21:38:54 +0200

    UserType

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType {
+    
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        
+        $builder
+                ->add('username', TextType::class)
+                ->add('plainPassword', RepeatedType::class, array(
+                    'type'=>  PasswordType::class,
+                    'first_options' => array('label'=>'Password'),
+                    'second_options'=> array('label'=>'Repeat Password'),
+                ));
+    }
+    
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+           'data_class' => 'UserBundle\Entity\Usuario' 
+        ));
+    }
+}
